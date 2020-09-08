b66d1f2591175cdb4e7607db8fcf28d1ed4c45b5
MaxDevProjects <proad.maxime.martin@gmail.com>
2020-09-08 10:19:19 +0200

    separation des valeur de l'accés à l'api du code initial dans la class

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 const axios = require('axios');
+import {ApiReader} from './constants'
+
 
 export default {
   name: 'App',
@@ -22,7 +24,7 @@ export default {
   },
   mounted() {
     axios
-        .get("https://unick-orn-lumag.go.yj.fr/index.php/wp-json/wc/v3/products?consumer_key=ck_ae09e58398fd465b7d796fa9713b55b3061c9575&consumer_secret=cs_45bc57883d05452f4bb658450a0e7d517ab1af0d")
+        .get(`${ApiReader.BASE_URL}${ApiReader.GET_ALL_PRODUCTS}?${ApiReader.CLIENT_KEY}&${ApiReader.SECRET_KEY}`)
         .then(response => (this.products = response))
   }
 }
